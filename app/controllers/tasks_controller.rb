c24299af07ca17e9c4fb4906255f9c2fb80ca5ed
jun kobayashi <junkobayashi1025@gmail.com>
2021-01-18 02:53:45 +0000

    新規登録画面を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,15 @@
 class TasksController < ApplicationController
-  def index
+  def new
+    @task = Task.new
+  end
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_url, notice: "新しいWhisperを登録しました"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
